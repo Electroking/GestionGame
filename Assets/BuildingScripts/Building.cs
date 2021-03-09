542cad6965e98b00b9121d3d2ce269c9c7b51cb4
Tsuki <maela.le-gall@e-artsup.net>
2021-03-09 17:13:15 +0100

    bâtiments début

---
+++ Assets/BuildingScripts/Building.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Building : MonoBehaviour
+{
+
+    public static int nbHouses;
+    public bool canbeBuilt;
+    public bool isBuilt;
+
+    enum BuildingType
+    {
+        House, School, Farm, Museum, Library
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    protected void OnBuilt()
+    {
+
+    }
+
+    public void Place()
+    {
+        isBuilt = true;
+    }
+
+    public void Build()
+    {
+        StartCoroutine(nameof(Construct), 6f);
+    }
+
+    IEnumerator Construct(float nbSeconds)
+    {
+        float seconds = 0;
+        while (seconds<nbSeconds)
+        {
+            seconds += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Village")
+        {
+            canbeBuilt = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Village")
+        {
+            canbeBuilt = false;
+        }
+    }
+}
---
+++ Assets/BuildingScripts/School.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class School : Building
+{
+    // Start is called before the first frame update
+    //public List<Villager> students = new List<Villager>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   /* public void ChangeJob(Villager villager)
+    {
+
+    }*/
+}
