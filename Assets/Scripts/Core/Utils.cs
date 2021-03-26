6d34382406c7d097b47e1c7c508195fc7c795455
Electroking <gwendo.leme@gmx.com>
2021-03-26 17:06:39 +0100

    nomprenomlist

--- Assets/Scripts/Core/Utils.cs
+++ Assets/Scripts/Core/Utils.cs
@@ -7,7 +7,13 @@ using UnityEngine.EventSystems;
 public static class Utils {
     public const string LAYER_BUILDING = "Building", LAYER_VILLAGER = "Villager", LAYER_UI = "UI", LAYER_ENVIRONMENT = "Environment", LAYER_TERRAIN = "Terrain";
     public const string TAG_TREE = "Tree", TAG_MINE = "Mine", TAG_BUSH = "Bush";
-
+    public static List<string> firstnameList = new List<string>() { "Gabriel", "Léo", "Raphaël", "Arthur", "Louis", "Emma", "Jade", "Louise", "Lucas",
+    "Adam", "Maël", "Jules", "Hugo", "Alice", "Liam", "Lina", "Chloé", "Noah", "Ethan", "Paul", "Mia", "Inès", "Léa", "Tiago", "Rose", "Mila", "Ambre", "Sacha",
+    "Gabin", "Nathan", "Mohamed", "Anna", "Aaron", "Eden", "Julia", "Léna", "Tom", "Noé", "Théo", "Elena", "Léon", "Zoé", "Juliette", "Manon", "Martin", "Mathis", "Eva", "Timéo", "Nolan", "Agathe"};
+    public static List<string> nameList = new List<string>() { "Martin", "Bernard", "Thomas", "Petit", "Robert", "Richard", "Durand", "Dubois", "Moreau", "Laurent",
+        "Simon", "Michel", "Lefèvre", "Leroy", "Roux", "David", "Bertrand", "Morel", "Fournier", "Girard", "Bonnet", "Dupont", "Lambert", "Fontaine", "Rousseau", 
+        "Vincent", "Muller", "Lefevre", "Faure", "Andre", "Mercier", "Blanc", "Guerin", "Boyer", "Garnier", "Chevalier", "Francois", "Legrand", "Gauthier", 
+        "Garcia", "Perrin", "Robin", "Clement", "Morin", "Nicolas", "Henry", "Roussel", "Mathieu", "Gautier", "Masson"};
     public static bool IsPointerOverUIElement() {
         // setup the pointer pos and event system
         var eventData = new PointerEventData(EventSystem.current);
