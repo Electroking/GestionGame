808947f49b5f8cbf9aff9d9e7f254739f6e6a2d0
ArthurGICQUEL <arthurgcl@orange.fr>
2021-03-29 11:22:43 +0200

    UIVillagerInfo cleaning01

--- Assets/Scripts/UI/UIVillagerInfos.cs
+++ Assets/Scripts/UI/UIVillagerInfos.cs
@@ -11,7 +11,7 @@ public class UIVillagerInfos : MonoBehaviour
 
     private void Update()
     {
-        if(villager == null) return;
+        if (villager == null) return;
         villagerNameTxt.text = villager.name;
     }
 
@@ -20,5 +20,10 @@ public class UIVillagerInfos : MonoBehaviour
         villager.AssignJob(Job.Type.Student);
         Student student = (Student)villager.job;
         student.jobToLearn = (Job.Type)job;
+    }
+
+    public void OnFirstSchoolBuilt()
+    {
+        GetComponentInChildren<Button>().interactable = true;
     }
 }
